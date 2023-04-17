2e1d05f1f83817015cebe84d2940593d0795adab
YACRUZ <yamatsukino@gmail.com>
2023-04-16 20:53:19 -0600

    create rick and morty

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import imgRickMorty from "./img/rick-morty.png";
+import imgCredencial from "./img/Cre.jpeg";
+import Characters from "./components/Characters";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [characters, setCharacters] = useState(null);
+
+  const reqApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    const characterApi = await api.json();
+    setCharacters(characterApi.results);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          YACRUZ MATSUKI...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title">Rick & Morty</h1>
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters} />
+        ) : (
+          <>
+            <img src={imgRickMorty} alt="Rick & Morty" className="img-home" />
+            <button onClick={reqApi} className="btn-search">
+              Buscar Personajes
+            </button>
+          </>
+        )}
+        <footer>
+          <p>Creado por: 
+            Yahir Jesus Jacome Cogco
+            
+          </p>
+          <div>
+            <a href="https://www.facebook.com/yahir.jesus.779"><img src="https://www.facebook.com/images/fb_icon_325x325.png" height="50px" width="50px" alt="Facebook"/></a>
+            <a href="https://www.youtube.com/channel/UC0nPwl62QIktOw3ZkktDHrw"><img src="https://www.transparentpng.com/thumb/youtube-logo/hd-youtube-logo-image-0.png" height="50px" width="50px" alt="YouTube"/></a>
+            <a href="https://github.com/YACRUZ"><img src="https://www.transparentpng.com/thumb/logos/github-logo-png-rtT9Sy.png" height="50px" width="50px" alt="GitHub"/></a>
+          </div>
+          <div>
+            <p>S20006732</p>
+            <img src={imgCredencial} height="230px" width="150px" alt="Credencial"/>
+          </div>
+        </footer>
       </header>
     </div>
+    
   );
 }
 
